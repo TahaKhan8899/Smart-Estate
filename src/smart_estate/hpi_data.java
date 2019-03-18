5ebf3a55139b7035d13102d18f0b61528759cc67
noa-barsky <barskyn@mcmaster.ca>
2019-03-18 14:13:13 -0400

    started parsing hpi dataset

---
+++ src/smart_estate/hpi_data.java
@@ -0,0 +1,27 @@
+package smart_estate;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class hpi_data {
+	
+	public static void read_data(String hpi_file) {
+		try {
+			br = new BufferedRedaer (new File)
+		}
+
+		
+	}
+
+	public static void main(String[] args) {
+//		try {
+//			read_data();
+//		} catch (FileNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			System.out.println("Did not work");
+//		}
+
+	}
+
+}
