ca7510d51ef1f0c157bcbdf1ff397c51372d324b
noa-barsky <barskyn@mcmaster.ca>
2019-03-18 14:20:30 -0400

    added data folder

--- src/smart_estate/hpi_data.java
+++ src/smart_estate/hpi_data.java
@@ -6,15 +6,28 @@ import java.io.IOException;
 
 public class hpi_data {
 	
-	public static void read_data(String hpi_file) {
+	public static void read_data(String hpi_file) throws IOException {
+		
 		try {
-			br = new BufferedRedaer (new File)
+			String line = "";
+			BufferedReader reader = new BufferedReader (new FileReader(hpi_file));
+			
+			
+//			while ((line = reader.readLine()) != null) {
+//				System.out.println(line);
+//				
+//			}
+			
+		} catch(FileNotFoundException e) {
+			System.out.println("did not find");
+			System.out.println(e);
 		}
 
 		
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		read_data("data/hpi.csv");
 //		try {
 //			read_data();
 //		} catch (FileNotFoundException e) {
