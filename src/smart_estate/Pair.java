fcb952b162974499bb09dd31155f00b9d97cde70
khant21 <khant21@mcmaster.ca>
2019-03-19 16:34:33 -0400

    finished Pair class and ReadHousingPrices.java

---
+++ src/smart_estate/Pair.java
@@ -0,0 +1,41 @@
+/**
+ * This is generic class for a pair that can hold two items
+ * @author Taha Khan
+ * Citation: https://stackoverflow.com/questions/9568403/how-to-store-an-array-of-pairs-in-java
+ */
+public class Pair {
+
+	private final String key;
+	private final double val;
+	
+	/**
+	 * This pair holds a State and a Value for that State 
+	 * @param state - String of the State
+	 * @param value - Double for price, crime, etc
+	 */
+	public Pair(String state, double value)
+	{
+		this.key = state;
+		this.val = value;
+	}
+	
+	/**
+	 * Get the key
+	 * @return - item 1
+	 */
+	public String state()   { return key; }
+	
+	/**
+	 * Get value
+	 * @return - item 2
+	 */
+    public double value()   { return val; }
+    
+    /**
+     * String representation of the pair
+     */
+    public String toString()
+	{
+		return ("State: " + this.key + ", Value: " + this.val);
+	}
+}
---
+++ src/smart_estate/ReadHousingPrices.java
@@ -0,0 +1,101 @@
+/**
+ * This module reads data and parses data from the Median Housing Prices dataset
+ * @author Taha Khan
+ *
+ */
+import java.io.*;
+import java.util.*;
+public class ReadHousingPrices {
+	
+	String fileName;
+	
+	/**
+	 * This is the constructor for reading housing prices  
+	 * @param name - name of the file to get housing prices from
+	 */
+	public ReadHousingPrices(String name) {
+		fileName = name;
+	}
+	
+	/**
+	 * It reads a csv file and parses it into an array of Pairs
+	 * @return array of Pairs - (State, Price)
+	 */
+	public Pair[] readPrices() {
+		
+		try {
+			
+			//open file
+			Scanner sc = new Scanner(new File(fileName));
+			List<String> lines = new ArrayList<String>();
+			
+			//add each line of the text file into an array
+			while (sc.hasNextLine()) {
+				  lines.add(sc.nextLine());
+			}
+			
+			//allLines is an array with each item being a row in the file  
+			String[] allLines = lines.toArray(new String[0]);
+			
+			Pair[] statesWithPrices = new Pair[allLines.length];
+			
+			//loop through all lines of the csv
+			for (int i = 0; i < allLines.length; i++)
+			{
+				//take a single line from the list of lines 
+				String fullLine = allLines[i];
+				
+				//get the State form the line
+				String state = parseLineForState(fullLine);
+				
+				//get the price from the line
+				double price = parseLineforPrice(fullLine);
+				
+				//create and add pair to list of pairs
+				Pair pair = new Pair(state, price);
+				statesWithPrices[i] = pair;
+			}
+			
+			return statesWithPrices;
+		}
+		catch(FileNotFoundException e)
+		{
+			Pair[] errorPair = new Pair[1];
+			return errorPair;
+		}
+	}
+	
+	/**
+	 * Parse line for the state
+	 * @param s - a line from the housingPrices csv
+	 * @return String for the state
+	 */
+	private String parseLineForState(String s)
+	{
+		//break it up on the ',"' and '",'
+		//this gives the form: "Alabama$132,100"
+		//take the first thing from that splitting, this is the State
+		String state = s.split("(,\")|(\",)")[0];
+		return state;
+	}
+	
+	/**
+	 * Parse line for the price
+	 * @param s - a line from the housingPrices csv
+	 * @return Double for the price
+	 */
+	private double parseLineforPrice(String s)
+	{
+		//break it up on the ',"' and '",'
+		//this gives the form: "Alabama$132,100"
+		//priceStr is of the form: '$132,100'
+		String priceStr = s.split("(,\")|(\",)")[1];
+		
+		//get rid of the '$' and ',' from the price and make it into a double
+		priceStr = priceStr.substring(1);
+		priceStr = priceStr.split(",")[0] + priceStr.split(",")[1];
+		double price = Double.parseDouble(priceStr);
+		return price;
+	}
+	
+}
