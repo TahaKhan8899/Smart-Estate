473daf5abaf89d3222559b8102487b8bb5952119
noa-barsky <barskyn@mcmaster.ca>
2019-03-20 22:17:34 -0400

    retrieved values, just need to put into array of tuples

--- src/smart_estate/hpi_data.java
+++ src/smart_estate/hpi_data.java
@@ -11,14 +11,15 @@ public class hpi_data {
 	public static void read_data(String hpi_file) throws IOException {
 		
 		try {
+			ArrayList<Double> base_years = new ArrayList<Double>();
+			ArrayList<Double> curr_years = new ArrayList<Double>();
 			
 			BufferedReader reader = new BufferedReader (new FileReader(hpi_file));
 			String line = reader.readLine();
-			Double base_year = 0.0;
-			Double curr_year = 0.0;
 			Double difference;
+			
+			int i = 0;
 			while (line != null) {
-				
 				String[] comma_seperated = line.split(",");
 				
 				
@@ -27,25 +28,21 @@ public class hpi_data {
 					
 					
 					if (comma_seperated[3].equals("2007") && comma_seperated[4].equals("4")) {
-//						base_year =  );
-						indexes.put(comma_seperated[2],Double.valueOf(comma_seperated[5]));
+						base_years.add(Double.valueOf(comma_seperated[5]));
 						
 					} 
-//					else if (comma_seperated[3].equals("2017") && comma_seperated[4].equals("4")){
-//						curr_year = Double.valueOf(comma_seperated[5] );
-//					}
-//					difference = curr_year - base_year;
-//					indexes.put(comma_seperated[2], difference);
-//					difference = 0.0;
-					
-				}
-				
+					else if (comma_seperated[3].equals("2017") && comma_seperated[4].equals("4")){
+						curr_years.add(Double.valueOf(comma_seperated[5]));
+						difference = Math.abs(curr_years.get(i) - base_years.get(i));
+						indexes.put(comma_seperated[2],difference);
+						i++;
+					}
 
-						
+					
+				}		
 				line = reader.readLine();
 				
 			}
-			
 			
 			
 			
@@ -58,9 +55,10 @@ public class hpi_data {
 			    count += 1;
 			    System.out.println ("Key: " + key + " Value: " + value);
 			}
+
+
 			System.out.println ("-----------");
 			System.out.println (count);
-			
 			
 			
 		} catch(FileNotFoundException e) {
