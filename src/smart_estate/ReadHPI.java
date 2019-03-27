1aba50c812625a147eb96be056b304da173461d8
noa-barsky <barskyn@mcmaster.ca>
2019-03-27 00:05:29 -0400

    completed sort module, just need to edit

--- src/smart_estate/ReadHPI.java
+++ src/smart_estate/ReadHPI.java
@@ -1,9 +1,9 @@
 /**
  * This module reads from HPI dataset
-
  * @author Noa Barsky
  *
  */
+
 package smart_estate;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
@@ -17,75 +17,48 @@ import java.util.*;
 public class ReadHPI{
 	
 	/**
-	 * This is the constructor for reading HPI
-	 * @param hpi_file - hpi_fileof the file where HPI for different states is
+	 * @brief Reads the data into an array of Pairs called Indexes
+	 * @param hpi_file a general parameter
 	 */
 	
 	public static Pair[] read_data(String hpi_file) throws IOException {
-		Pair[] indexes = new Pair[50];
-		try {
-			ArrayList<Double> base_years = new ArrayList<Double>();
-			ArrayList<Double> curr_years = new ArrayList<Double>();
+		Pair[] indexes = new Pair[50]; // An array of Pairs that contains the state and it's HPI
+		try { //if the file the found
+			ArrayList<Double> base_years = new ArrayList<Double>(); //Array of base years to compare with
+			ArrayList<Double> curr_years = new ArrayList<Double>(); //Array of current years
 			
-			BufferedReader reader = new BufferedReader (new FileReader(hpi_file));
-			String line = reader.readLine();
-			Double difference;
+			BufferedReader reader = new BufferedReader (new FileReader(hpi_file)); //Reader to read the CSV
+			String line = reader.readLine(); //The initial line of the CSV
+			Double difference; //A variable representing the difference between the base year and current year
 			
 			int i = 0;
-			while (line != null) {
-				String[] comma_seperated = line.split(",");
+			while (line != null) { //Loop to iterate over file
 				
+				String[] comma_seperated = line.split(","); // separating each line by by its fields
 				
-				if (comma_seperated.length == 6) {	
-
+				if (comma_seperated.length == 6) { //insurances it's a valid line
 					
-					
-					if (comma_seperated[3].equals("2007") && comma_seperated[4].equals("4") && !comma_seperated[2].equals("District of Columbia")) {
+					if (comma_seperated[3].equals("2007") 
+					    && comma_seperated[4].equals("4") 
+					    && !comma_seperated[2].equals("District of Columbia")) {
 						base_years.add(Double.valueOf(comma_seperated[5]));
-						
-					} 
-					else if (comma_seperated[3].equals("2017") && comma_seperated[4].equals("4") && !comma_seperated[2].equals("District of Columbia")){
-						curr_years.add(Double.valueOf(comma_seperated[5]));
-						difference = curr_years.get(i) - base_years.get(i);
-						indexes[i] = new Pair(comma_seperated[2], difference);
-						i++;
-					}
-
-					
+					} else if (comma_seperated[3].equals("2017") 
+							   && comma_seperated[4].equals("4") 
+							   && !comma_seperated[2].equals("District of Columbia")) {
+						           curr_years.add(Double.valueOf(comma_seperated[5]));
+						           difference = curr_years.get(i) - base_years.get(i);
+						           indexes[i] = new Pair(comma_seperated[2], difference);
+						           i++;
+				      }	
 				}		
 				line = reader.readLine();
-				
 			}
-			
-			
-			
-			int count = 0;
-			
-			for (int j = 0; j < indexes.length; j++ ) {
-				System.out.println(indexes[j].toString());
-				count++;
 				
-			}
-
-
-			System.out.println ("-----------");
-			System.out.println (count);
-			
-			
 		} catch(FileNotFoundException e) {
-			System.out.println("did not find");
 			System.out.println(e);
 		}
 
 		return indexes;
-	}
-
-	public static void main(String[] args) throws IOException {
-		read_data("data/hpi.csv");
-		Pair[] array = new Pair[50];
-		array = read_data("data/hpi.csv");
-		for (int i = 0; i < 50; i++)
-			System.out.print(array[i].toString()+"\n");
 	}
 
 }
--- src/smart_estate/Sort.java
+++ src/smart_estate/Sort.java
@@ -8,138 +8,90 @@
 
 package smart_estate;
 
-public class Sort
-{
-  /**
-   * Sort the object in ascending order.
-   * @param obj The object to be sorted
-   */
-  public static void sort(Comparable[] obj) 
-  {
-    shuffle(obj);
-    sort(obj, 0, obj.length - 1);
-  }
+import java.io.IOException;
+import edu.princeton.cs.algs4.StdRandom;
 
-  /**
-   * Sort the object with 3-way partitioning recursively.
-   * @param obj The object to be sorted
-   * @param l low index
-   * @param h high index
-   */
-  private static void sort(Comparable[] obj, int l, int h) 
-  { 
-    if(h <= l)
-      return;
-    int lesser = l;
-    int greater = h;
-    int i = l + 1;
-    while(i <= greater) 
-    {
-      if(lt(obj[i], obj[l])) 
-        swap(obj, lesser++, i);
-      else if(gt(obj[i], obj[l])) 
-        swap(obj, i, greater--);
-      else
-        i++;
-    }
-    sort(obj, l, lesser - 1);
-    sort(obj, greater + 1, h);
-  }
+public class Sort {
+	public static void sort(StateInfo[] a, String intent) {
+		StdRandom.shuffle(a); // Eliminate dependence on input.
+		sort(a, 0, a.length - 1, intent);
+	}
 
-  /**
-   * Test if v is less than w.
-   * @param v One element
-   * @param w Another element
-   * @return True if less than, false otherwise
-   */
-  private static boolean lt(Comparable v, Comparable w)
-  {
-    return v.compareTo(w) < 0;
-  }
+	private static void sort(StateInfo[] a, int lo, int hi, String intent) {
+		if (hi <= lo)
+			return;
+		int j = partition(a, lo, hi, intent); // Partition (see page 291).
+		sort(a, lo, j - 1, intent); 
+		sort(a, j + 1, hi, intent);
+	}
 
-  /**
-   * Test if v is greater than w.
-   * @param v One element
-   * @param w Another element
-   * @return True if greater than, false otherwise
-   */
-  private static boolean gt(Comparable v, Comparable w)
-  {
-    return v.compareTo(w) > 0;
-  }
-        
-  /**
-   * Swap obj[i] and obj[j].
-   * @param obj A list of objects
-   * @param i One index
-   * @param j Another index
-   */
-  private static void swap(Object[] obj, int i, int j) 
-  {
-    Object tmp = obj[i];
-    obj[i] = obj[j];
-    obj[j] = tmp;
-  }
+	private static int partition(StateInfo[] a, int lo, int hi, String intent) {
+		int i = lo, j = hi + 1; // left and right scan indices
+		StateInfo v = a[lo]; // partitioning item
+		while (true) { // Scan right, scan left, check for scan complete, and exchange.
+			while (less(a[++i], v, intent)) {
+				if (i == hi) {
+					break;
+				}
+			};
+			while (less(v, a[--j], intent)) {
+				if (j == lo) {
+					break;
+				}
+			};
+			if (i >= j) {
+				break;
+			}
+			exch(a, i, j);
+		}
+		exch(a, lo, j); // Put v = a[j] into position
+		return j;
+	}
 
-  /**
-   * Shuffle the object.
-   * @param obj The object to be shuffled
-   */
-  private static void shuffle(Comparable[] obj)
-  {
-    int n = obj.length;
-    for(int i = 0; i < n; i++) 
-    {
-      int j = i + (int) (Math.random() * (n - i));
-      swap(obj, i, j);
-    }
-  }
+	private static boolean less(StateInfo v, StateInfo w, String intent) {
+		if (intent.toLowerCase().equals("invest")) {
+			
+			return v.getHousingPrice() < w.getHousingPrice();	
+			
+		} else if (intent.toLowerCase().equals("sell")) {
+			
+			return v.getCrimeRate() < w.getCrimeRate();
+			
+		} else if (intent.toLowerCase().equals("buy")) {
+			
+			return v.getHPI() < w.getHPI();
+			
+		} 
+		System.out.println("Intent field filled out incorrectly");
+		return false;
 
-  // There are some useful functions for testing below. 
+		
+	}
 
-  /**
-   * Test if the object is sorted.
-   * @param obj The object to be tested
-   * @return True if sorted, false otherwise
-   */
-  private static boolean isSorted(Comparable[] obj) 
-  {
-    return isSorted(obj, 0, obj.length - 1);
-  }
+	private static void exch(StateInfo[] a, int i, int j) {
+		StateInfo t = a[i];
+		a[i] = a[j];
+		a[j] = t;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		StateInfo fst = new StateInfo("Alaska");
+		StateInfo snd = new StateInfo("Denver");
+		StateInfo thrd = new StateInfo("New York");
+		fst.setHousingPrice(6.0);
+		fst.setHPI(1.0);
+		snd.setHousingPrice(5.0);
+		snd.setHPI(2.0);
+		thrd.setHousingPrice(4.0);
+		thrd.setHPI(3.0);
+		StateInfo[] sample_lst = {fst, snd, thrd};
+		sort(sample_lst, "buy");
+		for (int i = 0; i < sample_lst.length; i++) {
+			System.out.println(sample_lst[i]);
+			
+		}
 
-  /**
-   * Test if the object is sorted.
-   * @param obj The object to be tested
-   * @return True if sorted, false otherwise
-   */
-  private static boolean isSorted(Comparable[] obj, int l, int h) 
-  {
-    for(int i = l + 1; i <= h; i++)
-      if(lt(obj[i], obj[i - 1])) 
-        return false;
-    return true;
-  }
+	}
+	
 
-  /**
-   * Print the object.
-   * @param obj The object to be printed
-   */
-  private static void printArray(Comparable[] obj) 
-  {
-    for (int i = 0; i < obj.length; i++)
-      System.out.print(obj[i] + " ");
-    System.out.println("");
-  }
-
-  public static void main(String[] args)
-  {
-    Integer[] list = {7, 4, 2, 3, 5, 1, 6}; 
-    printArray(list);
-    sort(list);
-    printArray(list);
-    if(isSorted(list))
-      System.out.println("The object is sorted.");
-    else
-      System.out.println("The object is not sorted.");
-  }
 }
